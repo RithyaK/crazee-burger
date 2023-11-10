df08ce436e500c7ea260f5ccb69814ddbe5ac6ff
Rithya <rithya@hotmail.fr>
2023-11-10 22:32:45 +0100

    F04 : OrderPage > Split Components Fix*

--- src/components/pages/orderpage/OrderPage.jsx
+++ src/components/pages/orderpage/OrderPage.jsx
@@ -22,10 +22,4 @@ export default OrderPage;
 
 const ContainerStyle = styled.div`
   padding: 24px 56px;
-  .maincontent {
-    border-radius: 0px 0px 15px 15px;
-    background: #f5f5f7;
-    height: 85vh;
-    box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
-  }
 `;
