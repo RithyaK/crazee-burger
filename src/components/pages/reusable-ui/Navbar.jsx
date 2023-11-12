3cf65a555e27b4b3d378d223e7849e66319459ae
Rithya <rithya@hotmail.fr>
2023-11-12 04:21:46 +0100

    🔥 F04 : Remove NavbarLeft

--- src/components/pages/reusable-ui/Navbar.jsx
+++ src/components/pages/reusable-ui/Navbar.jsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
-import Navbarleft from "../orderpage/Navbarleft";
 import Navbarright from "../orderpage/Navbarright";
+import Logo from "./Logo";
+import { refreshPage } from "../../../utils/window";
 const Navbar = () => {
   return (
     <NavbarStyle className="navbar">
-      <Navbarleft />
+      <Logo onClick={refreshPage} className={"logo-orderpage"} />
       <Navbarright />
     </NavbarStyle>
   );
@@ -14,7 +15,18 @@ export default Navbar;
 
 const NavbarStyle = styled.div`
   border-radius: 15px 15px 0px 0px;
+  padding-left: 20px;
   background: #fff;
   display: flex;
   justify-content: space-between;
+  .logo-orderpage {
+    cursor: pointer;
+  }
+  .logo-orderpage h1 {
+    font-size: 36px;
+  }
+  .logo-orderpage img {
+    height: 60px;
+    width: 80px; // for Safari and Firefox
+  }
 `;
