8e1c16cc46719befd1f13f372d9553240d0c6682
Rithya <rithya@hotmail.fr>
2023-11-13 02:05:24 +0100

    ✅ F05 : layout

--- src/components/pages/orderpage/Main.jsx
+++ src/components/pages/orderpage/Main.jsx
@@ -1,13 +1,54 @@
 import styled from "styled-components";
+import { fakeMenu2 } from "../../../../fakeData/fakeMenu";
+import PrimaryButton from "../reusable-ui/PrimaryButton";
 const Main = () => {
-  return <MainStyle></MainStyle>;
+  return (
+    <MainStyle>
+      {fakeMenu2.map((burger) => (
+        <div className="card" key={burger.id}>
+          <img
+            src={burger.imageSource}
+            alt={burger.title}
+            width="200px"
+            height="145px"
+          />
+          <h1>{burger.title}</h1>
+          <div className="container-price-button">
+            <p>{burger.price.toFixed(2)}$</p>
+            <PrimaryButton label={"Ajouter"} />
+          </div>
+        </div>
+      ))}
+    </MainStyle>
+  );
 };
 
 export default Main;
 
 const MainStyle = styled.div`
+  padding: 50px 90px;
+  display: grid;
+  grid-template-columns: repeat(4, 25%);
   border-radius: 0px 0px 15px 15px;
   background: #f5f5f7;
-  height: 85vh;
+  height: 1300px;
+  background: blue;
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+  .card {
+    width: 200px;
+    height: 270px;
+    background: white;
+    border: 1px solid grey;
+    border-radius: 15px;
+    text-align: center;
+    padding: 50px 20px 10px 20px;
+    box-shadow: -8px 8px 20px 0px rgb(0 0 0 / 20%);
+    font-family: Amatic SC;
+  }
+  .container-price-button {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-family: none;
+  }
 `;
